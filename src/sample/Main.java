101f1e5c63955ff53a0a10fa4ed9995213c73518
pavel <acc.for.code.skill@gmail.com>
2022-04-10 19:46:57 +0300

    answer

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,9 +1,11 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,13 +13,15 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("chat");
+        primaryStage.setScene(new Scene(root, 250, 400));
         primaryStage.show();
+        primaryStage.setResizable(false);
     }
 
 
     public static void main(String[] args) {
         launch(args);
+
     }
 }
